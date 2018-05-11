b306390ad3cb5dbe4009205fe84e273b3882cf5b
macDing <905081994@qq.com>
2018-05-11 16:34:26 +0800

    接口更新

--- src/common/api.js
+++ src/common/api.js
@@ -1,9 +1,23 @@
 // 首页
 const GetBanner = 'index/findIndexBanner'
 const GetCategories = 'index/findCategoryList'
-const GetGoodsPage = '/index/findIndexGoodsPage'
+const GetGoodsPage = 'index/findIndexGoodsPage'
+// 商品详情
+const GetGoodsBanner = 'goods/goodsBanner/'
+const GetGoodsInfo = 'goods/findGoodsInfoByGoodsId/'
+// 商家
+const GetBusinessBanner = 'business/businessBanner/'
+const GetBusinessInfo = 'business/findBusinessInfo/'
+const GetBusinessPage = 'business/findBusinessInfoPage'
+const GetBusinessShopList = 'business/findShopInfoList/'
 module.exports = {
   GetBanner,
   GetCategories,
-  GetGoodsPage
+  GetGoodsPage,
+  GetGoodsBanner,
+  GetGoodsInfo,
+  GetBusinessBanner,
+  GetBusinessInfo,
+  GetBusinessPage,
+  GetBusinessShopList
 }
